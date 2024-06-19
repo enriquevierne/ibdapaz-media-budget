b1b6d143e359c7f86a31cc83eb0d73838a3adc8d
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2024-06-19 18:32:23 -0300

    add some things

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { supabase } from "../lib/supabaseClient";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/vue/24/outline";
 
 const formatter = new Intl.NumberFormat("pt-BR", {
   style: "currency",
@@ -12,6 +13,11 @@ const items = ref([]);
 const readAll = async () => {
   const { data } = await supabase.from("items").select("*");
   items.value = data;
+};
+
+const destroy = async (id) => {
+  const { error } = await supabase.from("items").delete().eq("id", id);
+  readAll()
 };
 
 onMounted(() => {
@@ -29,27 +35,40 @@ onMounted(() => {
         </div>
         <span>Descrição: {{ item.description }}</span>
         <span>Link: {{ item.link }}</span>
-        <div>
-          
+        <div class="flex items-center justify-end gap-4">
+          <PencilSquareIcon class="w-6 cursor-pointer" />
+          <TrashIcon @click="destroy(item.id)" class="w-6 cursor-pointer" />
         </div>
       </li>
     </ul>
     <ul class="flex flex-col w-full col-span-4 gap-2 p-4 shadow-md bg-gray-50 rounded-xl">
       <h2 class="w-full p-2 bg-gray-300 rounded-md">Orçamento 2</h2>
-      <li v-for="item in items" class="p-2 border rounded-md">
-        <span>{{ item.item }}</span>
-        <span>{{ formatter.format(item.price) }}</span>
-        <span>{{ item.description }}</span>
-        <span>{{ item.link }}</span>
+      <li v-for="item in items" class="flex flex-col w-full gap-2 p-2 border rounded-md">
+        <div class="flex items-center justify-between">
+          <span>Item: {{ item.item }}</span>
+          <span>Preço: {{ formatter.format(item.price) }}</span>
+        </div>
+        <span>Descrição: {{ item.description }}</span>
+        <span>Link: {{ item.link }}</span>
+        <div class="flex items-center justify-end gap-4">
+          <PencilSquareIcon class="w-6 cursor-pointer" />
+          <TrashIcon @click="destroy(item.id)" class="w-6 cursor-pointer" />
+        </div>
       </li>
     </ul>
     <ul class="flex flex-col w-full col-span-4 gap-2 p-4 shadow-md bg-gray-50 rounded-xl">
       <h2 class="w-full p-2 bg-gray-300 rounded-md">Orçamento 3</h2>
-      <li v-for="item in items" class="p-2 border rounded-md">
-        <span>{{ item.item }}</span>
-        <span>{{ formatter.format(item.price) }}</span>
-        <span>{{ item.description }}</span>
-        <span>{{ item.link }}</span>
+      <li v-for="item in items" class="flex flex-col w-full gap-2 p-2 border rounded-md">
+        <div class="flex items-center justify-between">
+          <span>Item: {{ item.item }}</span>
+          <span>Preço: {{ formatter.format(item.price) }}</span>
+        </div>
+        <span>Descrição: {{ item.description }}</span>
+        <span>Link: {{ item.link }}</span>
+        <div class="flex items-center justify-end gap-4">
+          <PencilSquareIcon class="w-6 cursor-pointer" />
+          <TrashIcon @click="destroy(item.id)" class="w-6 cursor-pointer" />
+        </div>
       </li>
     </ul>
   </div>
--- src/components/FormAddItem.vue
+++ src/components/FormAddItem.vue
@@ -9,28 +9,29 @@ const submit = async () => {
     .from("items")
     .insert([{ item: newObj.value.item, price: newObj.value.price, description: newObj.value.description, link: newObj.value.link }])
     .select();
+    
 };
 </script>
 
 <template>
   <form @submit.prevent="submit" class="grid grid-cols-12 gap-2 p-4 m-auto shadow-md bg-gray-50 rounded-xl text-slate-100">
     <div class="flex items-center w-full col-span-3 gap-2 p-2 bg-gray-300 border-2 border-gray-300 rounded-md focus-within:border-gray-600">
-      <label class="p-1 text-sm font-semibold bg-gray-100 rounded text-slate-900" for="Item">Item</label>
+      <label class="p-1 text-sm font-semibold rounded text-slate-900" for="Item">Item</label>
       <input class="w-full bg-transparent outline-none text-slate-900" type="text" v-model="newObj.item" />
     </div>
 
     <div class="flex items-center w-full col-span-2 gap-2 p-2 bg-gray-300 border-2 border-gray-300 rounded-md focus-within:border-gray-600">
-      <label class="p-1 text-sm font-semibold bg-gray-100 rounded text-slate-900" for="jack">Preço</label>
+      <label class="p-1 text-sm font-semibold rounded text-slate-900" for="jack">Preço</label>
       <input class="w-full bg-transparent outline-none text-slate-900" type="number" v-model="newObj.price" />
     </div>
 
     <div class="flex items-center w-full col-span-3 gap-2 p-2 bg-gray-300 border-2 border-gray-300 rounded-md focus-within:border-gray-600">
-      <label class="p-1 text-sm font-semibold bg-gray-100 rounded text-slate-900" for="john">Descrição</label>
+      <label class="p-1 text-sm font-semibold rounded text-slate-900" for="john">Descrição</label>
       <input class="w-full bg-transparent outline-none text-slate-900" type="text" v-model="newObj.description" />
     </div>
 
     <div class="flex items-center w-full col-span-4 gap-2 p-2 bg-gray-300 border-2 border-gray-300 rounded-md focus-within:border-gray-600">
-      <label class="p-1 text-sm font-semibold bg-gray-100 rounded text-slate-900" for="mike">Link</label>
+      <label class="p-1 text-sm font-semibold rounded text-slate-900" for="mike">Link</label>
       <input class="w-full bg-transparent outline-none text-slate-900" type="text" v-model="newObj.link" />
     </div>
     <button class="col-span-2 col-start-11 p-2 text-sm font-bold tracking-wide uppercase bg-gray-800 rounded-md text-slate-100">adicionar</button>
